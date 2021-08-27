71cdd1594553ab108c25c0aba25c021294b863bf
tonovarela <tonovarela@live.com>
2021-08-26 21:11:12 -0500

    Se agrega prototipo del proyecto

---
+++ src/helpers/getPokemonOptions.js
@@ -0,0 +1,34 @@
+import pokemonApi from "../api/pokemonApi";
+
+const getPokemons = ()=>{
+
+    const pokemonsArr = Array.from(Array(650));
+    return pokemonsArr.map( (_,index)=> index+1 )    
+}
+
+const getPokemonOptions = async()=>{
+   const mixedPokemons = getPokemons()
+                         .sort( () => Math.random( ) - 0.5 );
+    const pokemonNames = await getPokemonNames( mixedPokemons.splice(0,4) );                             
+    console.table(pokemonNames);
+}
+
+const getPokemonNames =async([a,b,c,d]) =>{
+    
+    const arrayPromises= [
+        pokemonApi.get(`/${a}`),
+        pokemonApi.get(`/${b}`),
+        pokemonApi.get(`/${c}`),
+        pokemonApi.get(`/${d}`),
+    ];
+    const [poke1, poke2, poke3, poke4]  = await Promise.all(arrayPromises);
+   return [
+       { name : poke1.data.name, id:poke1.data.id},
+       { name : poke2.data.name, id:poke2.data.id},
+       { name : poke3.data.name, id:poke3.data.id},
+       { name : poke4.data.name, id:poke4.data.id},
+   ]
+}
+
+
+export default  getPokemonOptions;
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <h1>Quien es este pokemon?</h1>
+
+     <pokemon-picture  :pokemonId="98" :showPokemon="true" ></pokemon-picture>
+     <pokemon-options></pokemon-options>
+
+      
+      
+  </div>
+</template>
+
+<script>
+
+import PokemonOptions from '@/components/PokemonOptions.vue'
+import PokemonPicture from '@/components/PokemonPicture.vue'
+
+import getPokemonOptions  from  '@/helpers/getPokemonOptions';
+getPokemonOptions();
+
+export default {
+  components:{PokemonOptions,PokemonPicture}
+
+}
+</script>
+
+<style>
+
+</style>
